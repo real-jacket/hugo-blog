ffb23970465353260fa54e40557ab3a043a1a6cf
GitHub Action <action@github.com>
2022-03-28 13:41:42 +0800

    fix: fix vercel

--- vercel.init.bash
+++ vercel.init.bash
@@ -1,8 +1,8 @@
 git clone https://github.com/Lruihao/FixIt.git FixIt
+rm -rf FixIt/.git
 rm -rf FixIt/exampleSite
 git clone https://github.com/real-jacket/hugo-blog.git FixIt/exampleSite
 rm -rf FixIt/exampleSite/.git
-rm -rf FixIt/.git
-cp -rf .git FixIt/.git
+cp -rf .git FixIt/exampleSite/.git
 cd FixIt
 git checkout $VERCEL_GIT_COMMIT_SHA
