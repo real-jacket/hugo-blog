02cf66fff71b02fba445f0e3977eb7ccee8e1238
GitHub Action <action@github.com>
2022-05-15 00:45:01 +0800

    ci(vercel): fix

--- vercel.bash
+++ vercel.bash
@@ -5,4 +5,7 @@ git config --global core.quotePath false
 git config --global core.autocrlf false
 git config --global core.safecrlf true
 git config --global core.ignorecase false
+git ls-files -z | while read -d '' path; do
+    touch -d "$(git log -1 --format="@%ct" "$path")" "$path";
+done
 hugo -v --minify --gc
